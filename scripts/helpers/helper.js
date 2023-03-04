2b28a3497c92562383192b9480659790322d9bee
Evan Luo <luojiashao@gmail.com>
2023-03-04 14:03:37 -0500

    cdn: change source

--- scripts/helpers/helper.js
+++ scripts/helpers/helper.js
@@ -74,10 +74,27 @@ hexo.extend.helper.register('getPostUrl', function (rootUrl, path) {
 hexo.extend.helper.register('__js', function (path) {
   const _js = hexo.extend.helper.get('js').bind(hexo);
   const cdnPathHandle = (path_2) => {
-    return this.theme.cdn.enable
-      ? `<script src="//evan.beee.top/projects/hexo-theme-redefine/v${this.theme.version}/source/${path_2}"></script>`
+    if (this.theme.cdn.use == "unpkg") {
+      return this.theme.cdn.enable
+        ? `<script src="https://unpkg.com/hexo-theme-redefine@${this.theme.version}/source/${path_2}"></script>`
+        // ${this.theme.version}
+        : _js(path_2);
+    } else if (this.theme.cdn.use == "jsdelivr") {
+      return this.theme.cdn.enable
+        ? `<script src="https://cdn.jsdelivr.net/npm/hexo-theme-redefine@${this.theme.version}/source/${path_2}"></script>`
+        // ${this.theme.version}
+        : _js(path_2);
+    } else if (this.theme.cdn.use == "aliyun") {
+      return this.theme.cdn.enable
+        ? `<script src="//evan.beee.top/projects/hexo-theme-redefine/v${this.theme.version}/source/${path_2}"></script>`
+        // ${this.theme.version}
+        : _js(path_2);
+    } else {
+      return this.theme.cdn.enable
+      ? `<script src="https://unpkg.com/hexo-theme-redefine@${this.theme.version}/source/${path_2}"></script>`
       // ${this.theme.version}
       : _js(path_2);
+    }
   }
 
   let t = ``;
@@ -95,9 +112,19 @@ hexo.extend.helper.register('__js', function (path) {
 
 hexo.extend.helper.register('__css', function (path) {
   const _css = hexo.extend.helper.get('css').bind(hexo);
+  
   if (this.theme.cdn.enable) {
-    return `<link rel="stylesheet" href="//evan.beee.top/projects/hexo-theme-redefine/v${this.theme.version}/source/${path}">`;
+    if (this.theme.cdn.use == "unpkg") {
+      return `<link rel="stylesheet" href="https://unpkg.com/hexo-theme-redefine@${this.theme.version}/source/${path}">`;
+    } else if (this.theme.cdn.use == "jsdelivr") {
+      return `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/hexo-theme-redefine@${this.theme.version}/source/${path}">`;
+    } else if (this.theme.cdn.use == "aliyun") {
+      return `<link rel="stylesheet" href="//evan.beee.top/projects/hexo-theme-redefine/v${this.theme.version}/source/${path}">`;
+    } else {
+      return `<link rel="stylesheet" href="https://unpkg.com/hexo-theme-redefine@${this.theme.version}/source/${path}">`;
+    }
   } else {
     return _css(path);
   }
+
 });
