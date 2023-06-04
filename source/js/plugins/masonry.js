1ea5831b0713777c5f82c2593edf3d564e2510da
Evan Luo <contact@ohevan.com>
2023-06-04 11:11:59 -0400

    masonry js: optimize code

--- source/js/plugins/masonry.js
+++ source/js/plugins/masonry.js
@@ -1,42 +1,46 @@
 Global.initMasonry = () => {
-    var loadingPlaceholder = document.querySelector(".loading-placeholder");
-    var masonryContainer = document.querySelector("#masonry-container");
-    if (!loadingPlaceholder || !masonryContainer) return;
-  
-    loadingPlaceholder.style.display = "block";
-    masonryContainer.style.display = "none";
-  
-    var images = document.querySelectorAll("#masonry-container .masonry-item img");
-    var loadedCount = 0;
-    for (var i = 0; i < images.length; i++) {
-      var img = images[i];
-      if (img.complete) {
-        loadedCount++;
-      } else {
-        img.addEventListener("load", function () {
-          loadedCount++;
-          if (loadedCount === images.length) {
-            initializeMasonryLayout();
-          }
-        });
-      }
-    }
-  
+  var loadingPlaceholder = document.querySelector(".loading-placeholder");
+  var masonryContainer = document.querySelector("#masonry-container");
+  if (!loadingPlaceholder || !masonryContainer) return;
+
+  loadingPlaceholder.style.display = "block";
+  masonryContainer.style.display = "none";
+
+  var images = document.querySelectorAll("#masonry-container .masonry-item img");
+  var loadedCount = 0;
+
+  function onImageLoad() {
+    loadedCount++;
     if (loadedCount === images.length) {
       initializeMasonryLayout();
     }
-  
-    function initializeMasonryLayout() {
-      loadingPlaceholder.style.display = "none";
-      masonryContainer.style.display = "block";
-  
-      var masonry = new MiniMasonry({
-        container: masonryContainer,
-        gutterX: 10,
-        gutterY: 5,
-        surroundingGutter: false,
-      });
-      masonry.layout();
+  }
+
+  for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+    if (img.complete) {
+      onImageLoad();
+    } else {
+      img.addEventListener("load", onImageLoad);
     }
   }
+
+  if (loadedCount === images.length) {
+    initializeMasonryLayout();
+  }
+
+  function initializeMasonryLayout() {
+    loadingPlaceholder.style.display = "none";
+    masonryContainer.style.display = "block";
+
+    var masonry = new MiniMasonry({
+      container: masonryContainer,
+      gutterX: 10,
+      gutterY: 5,
+      surroundingGutter: false,
+    });
+    masonry.layout();
+  }
+}
+
   
