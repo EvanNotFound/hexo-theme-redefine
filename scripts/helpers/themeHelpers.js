eff5b6fb23faab99ff33d52d73c1dcf72eb1e919
Evan Luo <contact@ohevan.com>
2024-06-21 10:14:13 -0400

    refactor: simplify return logic in isInHomePaging helper function

--- scripts/helpers/themeHelpers.js
+++ scripts/helpers/themeHelpers.js
@@ -9,9 +9,9 @@ const themeVersion = version;
 hexo.extend.helper.register("isInHomePaging", function (pagePath, route) {
   if (pagePath.length > 5 && route === "/") {
     return pagePath.slice(0, 5) === "page/";
-  } else {
-    return false;
   }
+
+  return false;
 });
 
 /* code block language display */
