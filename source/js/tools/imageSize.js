ad5d24721fa7940af5cfdb4c932b7ad22fbe36b3
Prometheus17 <144680010+ElysiaFollower@users.noreply.github.com>
2025-09-20 17:54:32 +0800

    feat(imageSize): support parsing and get dimensions of JPEG(most, but not all).
    Based on  ITU-T T.81 standard. Application formats like JFIF and EXIF are supported. Correctness of application format that doesn't follow T.81 standard is not guaranteed

--- source/js/tools/imageSize.js
+++ source/js/tools/imageSize.js
@@ -1,22 +1,14 @@
 /**
  * @file imageSize.js
  * A image dimension parser.
- * Supports PNG and JPG formats.
+ * Supports PNG and JPEG formats.
  */
 
-// --- Constants for PNG Parsing ---
-// reference: https://www.w3.org/TR/png-3/#5DataRep
-// All PNG files start with this 8-byte signature.
-const PNG_SIGNATURE = Buffer.from([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]);
-// The IHDR chunk type identifier in ASCII.
-const IHDR_CHUNK_TYPE = Buffer.from('IHDR');
-// The IHDR chunk's data section must be 13 bytes long.
-const EXPECTED_IHDR_LENGTH = 13;
 
 /**
- * A function to get dimensions of PNG and JPG images from a Buffer.
+ * A function to get dimensions of PNG and JPEG images from a Buffer.
  * @param {Buffer} buffer The image data buffer.
- * @returns {{width: number, height: number, type: 'png' | 'jpg'} | null}
+ * @returns {{width: number, height: number, type: 'png' | 'jpeg'} | null}
  */
 function imageSize(buffer) {
   try {
@@ -26,9 +18,12 @@ function imageSize(buffer) {
       return pngResult;
     }
 
-    //JPG
+    //JPEG
+    const jpegResult = parseJPEG(buffer);
+    if(jpegResult){
+      return jpegResult;
+    }
 
-    
     console.error('Unsupported image format')
     return null; 
   } catch (e) {
@@ -38,6 +33,14 @@ function imageSize(buffer) {
 }
 
 
+// --- Constants for PNG Parsing ---
+// reference: https://www.w3.org/TR/png-3/#5DataRep
+// All PNG files start with this 8-byte signature.
+const PNG_SIGNATURE = Buffer.from([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]);
+// The IHDR chunk type identifier in ASCII.
+const IHDR_CHUNK_TYPE = Buffer.from('IHDR');
+// The IHDR chunk's data section must be 13 bytes long.
+const EXPECTED_IHDR_LENGTH = 13;
 /**
  * Parses a Buffer to extract dimensions of a PNG image.
  * @param {Buffer} buffer The buffer containing the image data.
@@ -83,17 +86,71 @@ function parsePNG(buffer) {
   }
 }
 
+
+// --- Constants for JPEG Parsing ---
+// reference: ITU T.81, Annex B  https://www.w3.org/Graphics/JPEG/itu-t81.pdf
+// Start of Image marker
+const JPG_SOI = 0xFFD8;
+// End of Image marker
+const JPG_EOI = 0xFFD9;
+
+// Start of Frame markers 
+//non-differential, Huffman coding
+const JPG_SOF0  = 0xFFC0; // Baseline DCT
+const JPG_SOF1  = 0xFFC1; // Extended sequential DCT
+const JPG_SOF2  = 0xFFC2; // Progressive DCT
+const JPG_SOF3  = 0xFFC3; // Lossless (sequential)
+//differential, Huffman coding
+const JPG_SOF5  = 0xFFC5; // sequential DCT
+const JPG_SOF6  = 0xFFC6; // progressive DCT
+const JPG_SOF7  = 0xFFC7; // lossless (sequential)
+//non-differential, arithmetic coding
+const JPG_SOF9  = 0xFFC9; // Extended sequential DCT
+const JPG_SOF10 = 0xFFCA; // Progressive DCT
+const JPG_SOF11 = 0xFFCB; // Lossless (sequential)
+//differential, arithmetic coding
+const JPG_SOF13 = 0xFFCD; // sequential DCT
+const JPG_SOF14 = 0xFFCE; // progressive DCT
+const JPG_SOF15 = 0xFFCF; // lossless (sequential)
+
+const SOF_MARKERS = [
+  JPG_SOF0, JPG_SOF1, JPG_SOF2, JPG_SOF3,
+  JPG_SOF5, JPG_SOF6, JPG_SOF7,
+  JPG_SOF9, JPG_SOF10, JPG_SOF11,
+  JPG_SOF13, JPG_SOF14, JPG_SOF15
+]
+
 /**
- * Parses a Buffer to extract dimensions of a JPG image.
+ * Parses a Buffer to extract dimensions of a JPEG image.
  * @param {Buffer} buffer The buffer containing the image data.
- * @returns {{width: number, height: number, type: 'jpg'} | null}
+ * @returns {{width: number, height: number, type: 'jpeg'} | null}
  */
-function parseJPG(buffer){
-  //TODO: implement
+function parseJPEG(buffer){
+  try{
+    //Start with SOI
+    if(buffer.readUInt16BE(0) !== JPG_SOI){
+      return null;
+    }
+
+    let offset = 2;
+    let marker = buffer.readUInt16BE(offset);
+    while(!SOF_MARKERS.includes(marker)){
+      //if not the SOF marker, skip to the next marker; note: RST_m will not occur before frame header, so we do not check for
+      length = buffer.readUInt16BE(offset+2);
+      offset += length + 2;
+      marker = buffer.readUInt16BE(offset);
+    }
+
+    //For frameheader: 2 for marker, 2 for length, 1 for precision, 2 for height, 2 for width
+    return { height: buffer.readUInt16BE(offset+5), width: buffer.readUInt16BE(offset+7), type: 'jpeg' };
+  }catch(e){
+    console.error(e);
+    return null;
+  }
 }
 
 module.exports = {
   imageSize,
   parsePNG, 
-  //parseJPG,
+  parseJPEG,
 };
